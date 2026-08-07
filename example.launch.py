98472c28c424a93c9c8230f2bb67df8c9eff39d6
Soonhong Park <168538598+hi-shp@users.noreply.github.com>
2026-08-07 16:41:36 +0900

    Update example.launch.py

--- example.launch.py
+++ example.launch.py
@@ -1,5 +1,3 @@
-cd example
-mkdir launch
 cat << 'EOF' > launch/example.launch.py
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -7,13 +5,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='py_pubsub',
+            package='example',
             namespace='',
             executable='listener',
             name='listener'
         ),
         Node(
-            package='py_pubsub',
+            package='example',
             namespace='',
             executable='talker',
             name='talker'
